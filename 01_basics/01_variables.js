8749a211c45f11af56c5e1ffc5d9063547dbf351
Rahim Ali <139705342+rahim1976@users.noreply.github.com>
2024-08-17 20:17:16 +0000

    learning datatypes here

--- 01_basics/01_variables.js
+++ 01_basics/01_variables.js
@@ -4,6 +4,8 @@ var accountPassword = "rahim123"
 accountCity = "karachi"
 
 // accountId = 2       Note:Overwritting not Not Allowed
+
+
 // updating values
 accountEmail = "hahd@google.com" //type: string
 accountPassword = "232323" // type: string
@@ -11,14 +13,8 @@ accountCity = "faislabad" // type: any
 let accountState; // undefined
 
 /*       
-prefer not to use var
-because of the issue in block scope and functional scope      
-*/
+Note:Prefer not to use var because of the issue in block scope and functional scope      
 
-/*console.log(accountId);             
-console.log(accountEmail);
-console.log(accountPassword);
-console.log(accountCity);
 Note: when you want to print multiple values use "console.table" command 
 */
 
