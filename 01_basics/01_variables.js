0df47f7f4bee0bfa5365ba9dc8652cd4e3ebe0a3
Rahim Ali <139705342+rahim1976@users.noreply.github.com>
2024-08-17 17:47:33 +0000

    1st output

--- 01_basics/01_variables.js
+++ 01_basics/01_variables.js
@@ -1,8 +1,19 @@
-const accountId = 1
+const accountId = 13242
 let accountEmail = "rahim@google.com"
 var accountPassword = "rahim123"
 accountCity = "karachi"
 
 // accountId = 2       Note:Overwritting not Not Allowed
+// updating values
+accountEmail = "hahd@google.com"
+accountPassword = "232323"
+accountCity = "faislabad"
 
-console.log(accountId);
+
+// console.log(accountId);             
+// console.log(accountEmail);
+// console.log(accountPassword);
+// console.log(accountCity);
+// Note: when you want to print multiple values use "console.table" command 
+
+console.table([accountId, accountEmail, accountPassword, accountCity]);
