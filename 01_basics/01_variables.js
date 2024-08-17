0b8d531c3dde78a29212cce3d12863539359522f
Rahim Ali <139705342+rahim1976@users.noreply.github.com>
2024-08-17 17:30:30 +0000

    variables

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,8 @@
+const accountId = 1
+let accountEmail = "rahim@google.com"
+var accountPassword = "rahim123"
+accountCity = "karachi"
+
+// accountId = 2       Note:Overwritting not Not Allowed
+
+console.log(accountId);
