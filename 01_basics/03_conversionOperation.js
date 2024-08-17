7d8ccaa098921262864bb65793822b6eaa84b5d5
Rahim Ali <139705342+rahim1976@users.noreply.github.com>
2024-08-17 22:23:52 +0000

    confusions of string to number conversion

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -28,5 +28,32 @@ let someNumber = "ali"
 
 
 let stringNumber = String(someNumber)
-console.log(stringNumber);
-console.log(typeof stringNumber);
+// console.log(stringNumber);
+// console.log(typeof stringNumber);
+
+
+// ****************** Operations ******************
+
+let value = 3
+let negValue = -value
+// console.log(negValue);
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**2);
+// console.log(2/3);
+// console.log(2%3);
+
+let str1 = "hello"
+let str2 = " rahim"
+
+let str3 = (str1+str2)
+console.log(str3);
+
+console.log("1" + 2);
+console.log(1 + "2");
+console.log("1" + 2 + 2);
+console.log(1 + 2 + "2");
+
+
