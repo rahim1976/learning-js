ceca433d281cba712527f6f0c5c2cd0be758b600
Rahim Ali <139705342+rahim1976@users.noreply.github.com>
2024-08-17 21:45:42 +0000

    conversion of datatypes complete

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,32 @@
+let score = true
+
+// console.log(typeof score);
+// console.log(typeof(score));
+
+let valueInNumber = Number(score)
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
+
+
+// "33"    => 33
+// "33abc" => Nan
+// true    => 1
+// false   => 0
+
+
+let isLoggedIn = null
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+// console.log(booleanIsLoggedIn);
+
+// 1        => true
+// 0        => false
+// "rahim"  => true
+// ""       => false
+
+let someNumber = "ali"
+
+
+let stringNumber = String(someNumber)
+console.log(stringNumber);
+console.log(typeof stringNumber);
