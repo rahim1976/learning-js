ecbf0d17e1e152b10931aaeb64efcdfc81a9cb23
Rahim Ali <139705342+rahim1976@users.noreply.github.com>
2024-08-17 18:15:53 +0000

    done variable classes

--- 01_basics/01_variables.js
+++ 01_basics/01_variables.js
@@ -5,15 +5,21 @@ accountCity = "karachi"
 
 // accountId = 2       Note:Overwritting not Not Allowed
 // updating values
-accountEmail = "hahd@google.com"
-accountPassword = "232323"
-accountCity = "faislabad"
+accountEmail = "hahd@google.com" //type: string
+accountPassword = "232323" // type: string
+accountCity = "faislabad" // type: any
+let accountState; // undefined
 
+/*       
+prefer not to use var
+because of the issue in block scope and functional scope      
+*/
 
-// console.log(accountId);             
-// console.log(accountEmail);
-// console.log(accountPassword);
-// console.log(accountCity);
-// Note: when you want to print multiple values use "console.table" command 
+/*console.log(accountId);             
+console.log(accountEmail);
+console.log(accountPassword);
+console.log(accountCity);
+Note: when you want to print multiple values use "console.table" command 
+*/
 
-console.table([accountId, accountEmail, accountPassword, accountCity]);
+console.table([accountId, accountEmail, accountPassword, accountCity, accountState]);
