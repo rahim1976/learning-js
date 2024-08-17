15aec35a5757afcb8e014096f695a925f0df7d4b
Rahim Ali <139705342+rahim1976@users.noreply.github.com>
2024-08-17 20:33:36 +0000

    datatypes finished

--- 01_basics/02_datatypes.js
+++ 01_basics/02_datatypes.js
@@ -2,5 +2,29 @@
 
 // alert(3+3)  we are using NodeJs, Not Browser
 
-console.log(3+3)
+console.log(3
+    +
+    3) // code readability should be high
  console.log("rahim")
+
+
+
+ let name = "rahim"
+ let age = "18"
+ let isLoggedIn = false
+ let state;
+
+ // PRIMITIVE DATATYPES
+ // number    => to power 53
+ // bigint    => for big integers
+ // string    => ""
+ // boolean   => true/false
+ // null      => StandAlone Value(Object)
+ // undefined => Value not defined like line no 15
+ // Symbol    => unique
+
+ //OBJECT
+
+ console.log(typeof undefined); // undefined
+ console.log(typeof null);      // object
+ 
