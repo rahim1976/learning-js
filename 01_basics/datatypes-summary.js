54da9034c7e352e3797cd34d041180d5732046a2
Rahim Ali <139705342+rahim1976@users.noreply.github.com>
2024-08-24 22:10:32 +0000

    changes made

--- 01_basics/datatypes-summary.js
+++ 01_basics/datatypes-summary.js
@@ -15,9 +15,28 @@ const anotherId = Symbol('123')
 
 console.log(id === anotherId);
 
+const bigNumber = 3284623423472347326423n
+
+
 
 
 
 // Reference (Non-Primitive)
 
 // Array, Objects, Functions
+
+
+
+const heros = ["Spiderman", "Superman", "batman"]
+let myDetails = {
+    name: "rahim",
+    age: "18",
+    country: "pakistan",
+}
+
+const myFunction  = function(){
+    console.log("hello world");
+    
+}
+
+console.log(typeof BigNumber);
