78d24d72c726a89df2e02b0459e474490c579e1c
Rahim Ali <139705342+rahim1976@users.noreply.github.com>
2024-09-10 22:14:51 +0000

    maths in js

---
+++ 01_basics/06_nums_and_math.js
@@ -0,0 +1,17 @@
+const score = 400
+// console.log(score);  Prints Number 
+
+const balance = new Number(100);
+// console.log(balance); Print Number Hardcoded
+
+// console.log(balance.toString().length); changes number to string and give character length
+// console.log(balance.toFixed(2)); fix the zero's given after decimal
+
+const otherNumber = 123.8966
+
+// console.log(otherNumber.toPrecision(3)); Precise the value to the closes round off
+
+
+const hundreds = 10000000
+console.log(hundreds.toLocaleString('en-PK'));
+
