5c58806d99af0617cbfc684eb4dd449e561e0280
Rahim Ali <139705342+rahim1976@users.noreply.github.com>
2024-12-02 21:10:29 +0000

    math and nums

--- 01_basics/06_nums_and_math.js
+++ 01_basics/06_nums_and_math.js
@@ -9,9 +9,41 @@ const balance = new Number(100);
 
 const otherNumber = 123.8966
 
-// console.log(otherNumber.toPrecision(3)); Precise the value to the closes round off
+// console.log(otherNumber.toPrecision(3)); //Precise the value to the closes round off
 
 
 const hundreds = 10000000
-console.log(hundreds.toLocaleString('en-PK'));
+// console.log(hundreds.toLocaleString('en-PK'));
+
+//  +++++++++++Maths++++++++++++++++
+
+
+// console.log(Math);
+// console.log(Math.abs(4)); // print 4
+// console.log(Math.round(4.6)); // .5 k bad round off kryga
+// console.log(Math.ceil(4.4)); // 4 se halka sa bhi bara huwa to 5 print kryga
+// console.log(Math.floor(4.9)); // 5 se halka sa bhi nechy huwa to 4 he print kryga
+// console.log(Math.min(4, 3, 2, 6)); // minimum value print kryga given no mei se
+// console.log(Math.max(4, 3, 2, 6)); // maximum value print kryga given no mei se
+
+
+console.log(Math.random());
+console.log((Math.random()*10) +1);
+console.log(Math.floor(Math.random()*10) +1);
+
+const min = 10
+const max = 20
+
+console.log(Math.floor(Math.random() * (max - min + 1)) + min);
+
+
+
+
+
+
+
+
+
+
+
 
